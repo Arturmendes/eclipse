c03d10dc2d922e27626b8e9eac084cdda2c1e684
Artur Mendes <arturmendessantos@hotmail.com>
2020-09-28 15:01:30 -0300

    outro teste

--- HelloGit/src/br/com/artur/git/HelloGit.java
+++ HelloGit/src/br/com/artur/git/HelloGit.java
@@ -9,6 +9,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("Artur Mendes Santos");
+		System.out.println("outro Teste");
 	}
 
 }
