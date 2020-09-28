11e7450cc90823d650a64054ea20f097f85ef128
Artur Mendes Santos <53579191+Arturmendes@users.noreply.github.com>
2020-09-28 14:24:08 -0300

    Adicionei arquivos via upload

---
+++ HelloGit/src/br/com/artur/git/HelloGit.java
@@ -0,0 +1,13 @@
+package br.com.artur.git;
+
+/*
+ * @author Artur Mendes
+ * Exemplo de integração do Eclipse com o GitHub
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+
+}
